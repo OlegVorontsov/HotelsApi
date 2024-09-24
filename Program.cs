4f680b8d972fdeca84369c4cf32662d28ac1e434
Олег Воронцов <oleg-voroncov@mail.ru>
2024-09-24 12:53:17 +0300

    class Hotel and CRUD operations with List added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.VisualBasic;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var hotels = new List<Hotel>();
+
+app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
+app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
+app.MapPut("/hotels", (Hotel hotel) =>
+  {
+    var index = hotels.FindIndex(h => h.Id == hotel.Id);
+    if (index < 0) throw new Exception("Not found");
+    hotels[index] = hotel;
+  });
+app.MapDelete("/hotels/{id}", (int id) =>
+{
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0) throw new Exception("Not found");
+    hotels.RemoveAt(index);
+});
 
 app.Run();
+
+public class Hotel
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = string.Empty;
+  public string Description { get; set; }
+  public string Address { get; set; }
+  public int StarsCount { get; set; }
+}
